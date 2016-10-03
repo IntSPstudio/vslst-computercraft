802e582d804e04baeadb2ba68c7b4a57a6ac7676
IntSPstudio <official.intsp@gmail.com>
2016-10-04 00:52:47 +0300

    New project name and feature

--- VSLstAd.lua
+++ VSLstAd.lua
@@ -1,8 +1,10 @@
 --[[
-Project: Underground bunker
+Project: Hidingcube
 Thank you for using this script!
 ]]--
 
+-- DIRECTION: 0=DOWN 1=UP 
+local vdir =1
 -- VARIABLES
 local range=0
 print("How big square? 3,5,7")
@@ -12,13 +14,20 @@ local height =3
 local path =3
 local turningchecka =0
 -- RULE CHECK
-if check > 0 then
+if check > 2 then
 	range = check
-	-- PATH
-	for i=0,path do
-		turtle.dig()
-		turtle.forward()
-		turtle.digUp()
+	if vdir == 1 then
+		-- PATH
+		for i=0,path do
+			turtle.dig()
+			turtle.forward()
+			turtle.digUp()
+		end
+	else
+		turtle.digDown()
+		turtle.down()
+		turtle.digDown()
+		turtle.down()
 	end
 	-- AREA
 	for i=1,height do
@@ -49,19 +58,28 @@ if check > 0 then
 		end
 		-- NEW LAYER
 		if i < height then
-			turtle.digUp()
-			turtle.up()
+			if vdir == 1 then
+				turtle.digUp()
+				turtle.up()
+			else
+				turtle.digDown()
+				turtle.down()
+			end
 			turtle.turnLeft()
 			turtle.turnLeft()
 			turningchecka =0
 		end
 	end
 	-- DOWN
-	check = turtle.detectDown()
-	while check == false do
-		turtle.down()
+	if vdir == 1 then
 		check = turtle.detectDown()
+		while check == false do
+			turtle.down()
+			check = turtle.detectDown()
+		end
 	end
+	-- READY
+	print("DONE")
 else
 	print("Nope")
 end
